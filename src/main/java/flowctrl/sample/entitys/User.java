77e593d0344f03fa95ca6ab650f1ebe12eb0ecb1
allbegray <allbegray@nate.com>
2016-04-29 11:39:01 +0900

    init

---
+++ src/main/java/flowctrl/sample/entitys/User.java
@@ -0,0 +1,75 @@
+package flowctrl.sample.entitys;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.AbstractAuditable;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+/**
+ * Created by allbegray on 2016-04-29.
+ */
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "login_id", unique = true)
+    private String loginId;
+
+    @Column(name = "password")
+    private String password;
+
+    @CreatedDate
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLoginId() {
+        return loginId;
+    }
+
+    public void setLoginId(String loginId) {
+        this.loginId = loginId;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ src/main/java/flowctrl/sample/service/security/SampleUserDetailsService.java
@@ -0,0 +1,29 @@
+package flowctrl.sample.service.security;
+
+import flowctrl.sample.entitys.User;
+import flowctrl.sample.modules.user.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * Created by allbegray on 2016-04-29.
+ */
+@Transactional
+public class SampleUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByLoginId(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found: " + username);
+        }
+        return new SampleUserDetails(user);
+    }
+
+}
