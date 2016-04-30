96b9e4f6f57a6684ad97df0946a864d0db17eb5f
allbegray <allbegray@gmail.com>
2016-04-30 17:29:54 +0900

    글로벌 컨트롤러 어드바이스 추가

---
+++ src/main/java/flowctrl/sample/modules/global/GlobalControllerAdvice.java
@@ -0,0 +1,26 @@
+package flowctrl.sample.modules.global;
+
+import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.InitBinder;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by allbegray on 2016-04-30.
+ */
+@ControllerAdvice
+public class GlobalControllerAdvice {
+
+    @InitBinder
+    public void dataBinding(WebDataBinder binder) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        dateFormat.setLenient(false);
+        binder.registerCustomEditor(Date.class, new CustomDateEditor(dateFormat, true));
+        binder.registerCustomEditor(String.class, new StringTrimmerEditor(true));
+    }
+
+}
