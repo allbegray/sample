d5aa77cc4fad18ce025c3feb578cea8d21717425
allbegray <allbegray@gmail.com>
2016-09-01 09:27:19 +0900

    Jooq 유틸 클래스 추가

---
+++ src/main/java/allbegray/sample/common/jooq/JooqUtils.java
@@ -0,0 +1,45 @@
+package allbegray.sample.common.jooq;
+
+import org.jooq.Field;
+import org.jooq.SortField;
+import org.jooq.impl.DSL;
+import org.springframework.data.domain.Sort;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+
+/**
+ * Created by allbegray on 2016-09-01.
+ */
+public class JooqUtils {
+
+    /**
+     * order 절에 사용될 정렬 필드 생성. 타입 세이프 하지는 않다.
+     * @param sortSpecification
+     * @return
+     */
+    public static Collection<SortField<?>> getSortFields(Sort sortSpecification) {
+        Collection<SortField<?>> querySortFields = new ArrayList<>();
+
+        if (sortSpecification == null) {
+            return querySortFields;
+        }
+
+        Iterator<Sort.Order> specifiedFields = sortSpecification.iterator();
+
+        while (specifiedFields.hasNext()) {
+            Sort.Order specifiedField = specifiedFields.next();
+
+            String sortFieldName = specifiedField.getProperty();
+            Sort.Direction sortDirection = specifiedField.getDirection();
+
+            Field<?> field = DSL.field(sortFieldName);
+            SortField<?> querySortField = sortDirection == Sort.Direction.ASC ? field.asc() : field.desc();
+            querySortFields.add(querySortField);
+        }
+
+        return querySortFields;
+    }
+
+}
---
+++ src/test/java/jooq/JooqTest.java
@@ -0,0 +1,50 @@
+package jooq;
+
+import org.jooq.DSLContext;
+import org.jooq.Record;
+import org.jooq.SQLDialect;
+import org.jooq.SelectConditionStep;
+import org.jooq.impl.DSL;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import static allbegray.sample.common.jooq.JooqUtils.getSortFields;
+
+/**
+ * Created by allbegray on 2016-09-01.
+ */
+public class JooqTest {
+
+    DSLContext create = null;
+    Connection conn = null;
+
+    @Before
+    public void setup() throws SQLException {
+        conn = DriverManager.getConnection("jdbc:h2:file:~/testdb", "sa", null);
+        create = DSL.using(conn, SQLDialect.H2);
+    }
+
+    @After
+    public void shutdown() throws SQLException {
+        conn.close();
+    }
+
+    @Test
+    public void test() throws SQLException {
+        PageRequest pageRequest = new PageRequest(0, 10, Sort.Direction.DESC, "title");
+
+        SelectConditionStep<Record> query = create.select().from(DSL.table("board")).where(DSL.field("title", String.class).like("제목%"));
+        query.orderBy(getSortFields(pageRequest.getSort()));
+
+        create.fetchCount(query);
+        query.fetch();
+    }
+
+}
